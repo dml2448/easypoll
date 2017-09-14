21e2470b2e71011200d58493f1e972dae3f5049c
DML <dml@symplatonix.com>
2017-09-14 19:33:12 +0200

    add empty routes and components
    Define main components and routes to fill out later

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import './App.css';
+
+import Questions from './Questions';
+import Poll from './Poll';
+import NewQuestion from './NewQuestion';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Questions} />
+            <Route exact path='/questions/create' component={NewQuestion} />
+            <Route exact path='/questions/:questionId' component={Poll} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/Poll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Poll extends Component {
+  render() {
+    return (
+      <div className="questionDetails">
+        Poll
+      </div>
+    )
+  }
+}
+
+export default Poll;
